a24122fa3d5c6bb29e560f584bd92e70370183e2
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-06-20 14:42:58 +0100

    Support injection of auxiliary classes, even in the bootstrap classloader.
    Uses instrumentation to install an access wrapper around `Unsafe.defineClass`
    
     * To use this feature, first call `ClassInjector.install(instrumentation)`
     * To inject a class, use `ClassInjector.defineClass(bytes, classloader)`
     * Use `defineBootClass` to inject classes into the bootstrap classloader
     * The API also supports injecting classes using a custom `ProtectionDomain`

---
+++ class-inject/src/main/java/datadog/instrument/clazz/ClassInjector.java
@@ -0,0 +1,206 @@
+package datadog.instrument.clazz;
+
+import static org.objectweb.asm.Opcodes.*;
+
+import datadog.instrument.utils.Platform;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+import java.util.function.BiFunction;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Type;
+
+/**
+ * Supports injection of auxiliary classes, even in the bootstrap classloader.
+ *
+ * <p>Uses {@link Instrumentation} to install an access wrapper around {@code Unsafe.defineClass}
+ *
+ * <ul>
+ *   <li>To use this feature, first call <code>ClassInjector.install(instrumentation)</code>
+ *   <li>To inject a class, use <code>ClassInjector.defineClass(bytecode, classloader)</code>
+ *   <li>Use <code>defineBootClass</code> to inject classes into the bootstrap classloader
+ *   <li>The API also supports injecting classes using a custom {@link ProtectionDomain}
+ * </ul>
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+public final class ClassInjector {
+
+  /** Injects a class in the bootstrap classloader. */
+  public static Class<?> injectBootClass(byte[] bytecode) {
+    return (Class<?>) classDefiner().apply(bytecode, null);
+  }
+
+  /** Injects a class in the specified classloader. */
+  public static Class<?> injectClass(byte[] bytecode, ClassLoader cl) {
+    return (Class<?>) classDefiner().apply(bytecode, cl);
+  }
+
+  /** Injects a class using the given protection domain. */
+  public static Class<?> injectClass(byte[] bytecode, ProtectionDomain pd) {
+    return (Class<?>) classDefiner().apply(bytecode, pd);
+  }
+
+  private static BiFunction classDefiner() {
+    if (classDefiner == null) {
+      throw new UnsupportedOperationException("ClassInjector is not installed");
+    }
+    return classDefiner;
+  }
+
+  private static volatile BiFunction classDefiner;
+
+  public static void install(Instrumentation inst) {
+    try {
+      InjectGlue injectGlue = new InjectGlue();
+      try {
+        // temporary transformation to install our glue to access Unsafe.defineClass
+        inst.addTransformer(injectGlue, true);
+        inst.retransformClasses(ClassLoader.class);
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+        classDefiner = (BiFunction) cl.loadClass(DefineClassGlue.ID).newInstance();
+      } finally {
+        inst.removeTransformer(injectGlue);
+        inst.retransformClasses(ClassLoader.class);
+      }
+    } catch (Throwable e) {
+      throw new UnsupportedOperationException("Unable to install ClassInjector", e);
+    }
+  }
+
+  static final class InjectGlue implements ClassFileTransformer {
+    @Override
+    public byte[] transform(
+        ClassLoader loader,
+        String className,
+        Class<?> classBeingRedefined,
+        ProtectionDomain protectionDomain,
+        byte[] bytecode) {
+      if ("java/lang/ClassLoader".equals(className)) {
+        ClassReader cr = new ClassReader(bytecode);
+        ClassWriter cw = new ClassWriter(cr, 0);
+        cr.accept(new ClassLoaderPatch(cw), 0);
+        return cw.toByteArray();
+      } else {
+        return null;
+      }
+    }
+  }
+
+  static final class ClassLoaderPatch extends ClassVisitor {
+    ClassLoaderPatch(ClassVisitor cv) {
+      super(ASM9, cv);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(
+        int access, String name, String descriptor, String signature, String[] exceptions) {
+      MethodVisitor mv = cv.visitMethod(access, name, descriptor, signature, exceptions);
+      // only transform the specific 'loadClass(name)' method we call to retrieve the injected glue
+      if ("loadClass".equals(name) && "(Ljava/lang/String;)Ljava/lang/Class;".equals(descriptor)) {
+        return new LoadClassPatch(mv);
+      }
+      return mv;
+    }
+  }
+
+  static final class LoadClassPatch extends MethodVisitor {
+    LoadClassPatch(MethodVisitor mv) {
+      super(ASM9, mv);
+    }
+
+    @Override
+    public void visitCode() {
+      mv.visitCode();
+
+      Label notDatadogGlueRequest = new Label();
+
+      // add branch at start of loadClass method to define our glue as a hidden/anonymous class
+      mv.visitLdcInsn(DefineClassGlue.ID);
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitMethodInsn(
+          INVOKEVIRTUAL, "java/lang/String", "equals", "(Ljava/lang/Object;)Z", false);
+      mv.visitJumpInsn(IFEQ, notDatadogGlueRequest);
+
+      if (Platform.atLeastJava(15)) {
+        // on Java 15+ prepare MethodHandles.lookup()
+        mv.visitMethodInsn(
+            INVOKESTATIC,
+            "java/lang/invoke/MethodHandles",
+            "lookup",
+            "()Ljava/lang/invoke/MethodHandles$Lookup;",
+            false);
+        mv.visitLdcInsn(DefineClassGlue.V9);
+      } else if (Platform.atLeastJava(9)) {
+        // on Java 9+ prepare jdk.internal.misc.Unsafe
+        mv.visitMethodInsn(
+            INVOKESTATIC,
+            "jdk/internal/misc/Unsafe",
+            "getUnsafe",
+            "()Ljdk/internal/misc/Unsafe;",
+            false);
+        mv.visitLdcInsn(Type.getType(ClassLoader.class));
+        mv.visitLdcInsn(DefineClassGlue.V9);
+      } else {
+        // on Java 8 prepare sun.misc.Unsafe
+        mv.visitMethodInsn(
+            INVOKESTATIC, "sun/misc/Unsafe", "getUnsafe", "()Lsun/misc/Unsafe;", false);
+        mv.visitLdcInsn(Type.getType(ClassLoader.class));
+        mv.visitLdcInsn(DefineClassGlue.V8);
+      }
+
+      // unpack the UTF-16BE encoded string back into bytecode
+      mv.visitFieldInsn(
+          GETSTATIC, "java/nio/charset/StandardCharsets", "UTF_16BE", "Ljava/nio/charset/Charset;");
+      mv.visitMethodInsn(
+          INVOKEVIRTUAL, "java/lang/String", "getBytes", "(Ljava/nio/charset/Charset;)[B", false);
+
+      if (Platform.atLeastJava(15)) {
+        // on Java 15+ use MethodHandles.lookup().defineHiddenClass(...)
+        mv.visitInsn(ICONST_0);
+        mv.visitInsn(ICONST_0);
+        mv.visitTypeInsn(ANEWARRAY, "java/lang/invoke/MethodHandles$Lookup$ClassOption");
+        mv.visitMethodInsn(
+            INVOKEVIRTUAL,
+            "java/lang/invoke/MethodHandles$Lookup",
+            "defineHiddenClass",
+            "([BZ[Ljava/lang/invoke/MethodHandles$Lookup$ClassOption;)Ljava/lang/invoke/MethodHandles$Lookup;",
+            false);
+        // use lookupClass() to retrieve the hidden class we just defined
+        mv.visitMethodInsn(
+            INVOKEVIRTUAL,
+            "java/lang/invoke/MethodHandles$Lookup",
+            "lookupClass",
+            "()Ljava/lang/Class;",
+            false);
+      } else if (Platform.atLeastJava(9)) {
+        // on Java 9+ use jdk.internal.misc.Unsafe.defineAnonymousClass(...)
+        mv.visitInsn(ACONST_NULL);
+        mv.visitMethodInsn(
+            INVOKEVIRTUAL,
+            "jdk/internal/misc/Unsafe",
+            "defineAnonymousClass",
+            "(Ljava/lang/Class;[B[Ljava/lang/Object;)Ljava/lang/Class;",
+            false);
+      } else {
+        // on Java 8 use sun.misc.Unsafe.defineAnonymousClass(...)
+        mv.visitInsn(ACONST_NULL);
+        mv.visitMethodInsn(
+            INVOKEVIRTUAL,
+            "sun/misc/Unsafe",
+            "defineAnonymousClass",
+            "(Ljava/lang/Class;[B[Ljava/lang/Object;)Ljava/lang/Class;",
+            false);
+      }
+
+      mv.visitInsn(ARETURN);
+
+      // otherwise this is a standard load request, handle it as before
+      mv.visitLabel(notDatadogGlueRequest);
+      mv.visitFrame(F_SAME, 0, null, 0, null);
+    }
+  }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "dd-instrument-java"
 
 include("utils")
+include("class-inject")
