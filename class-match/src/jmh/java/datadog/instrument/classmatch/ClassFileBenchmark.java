70208c21657009319d176ad017fc2f90dfb36949
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-07-31 11:22:18 +0100

    Re-use sample classes for other benchmarks

--- class-match/src/jmh/java/datadog/instrument/classmatch/ClassFileBenchmark.java
+++ class-match/src/jmh/java/datadog/instrument/classmatch/ClassFileBenchmark.java
@@ -1,20 +1,12 @@
 package datadog.instrument.classmatch;
 
 import static java.util.concurrent.TimeUnit.MICROSECONDS;
-import static java.util.stream.Collectors.toList;
 import static org.objectweb.asm.ClassReader.*;
 import static org.objectweb.asm.Opcodes.ASM9;
 import static org.openjdk.jmh.annotations.Mode.AverageTime;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.jar.JarFile;
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
@@ -41,31 +33,7 @@ public class ClassFileBenchmark {
 
   @Setup(Level.Trial)
   public void setup() {
-    File sampleJarFile = new File("sample.jar");
-    byte[] buf = new byte[16384];
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    try (JarFile sample = new JarFile(sampleJarFile)) {
-      bytecodes =
-          sample.stream()
-              .filter(e -> e.getName().endsWith(".class"))
-              .map(
-                  e -> {
-                    out.reset();
-                    try (InputStream in = sample.getInputStream(e)) {
-                      int nRead;
-                      while ((nRead = in.read(buf, 0, buf.length)) != -1) {
-                        out.write(buf, 0, nRead);
-                      }
-                      return out.toByteArray();
-                    } catch (IOException ignore) {
-                      return null;
-                    }
-                  })
-              .filter(Objects::nonNull)
-              .collect(toList());
-    } catch (IOException ex) {
-      throw new UncheckedIOException(ex);
-    }
+    bytecodes = SampleClasses.load();
   }
 
   @Benchmark
---
+++ class-match/src/jmh/java/datadog/instrument/classmatch/SampleClasses.java
@@ -0,0 +1,40 @@
+package datadog.instrument.classmatch;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+
+final class SampleClasses {
+  public static List<byte[]> load() {
+    File sampleJarFile = new File("sample.jar");
+    byte[] buf = new byte[16384];
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    try (JarFile sample = new JarFile(sampleJarFile)) {
+      return sample.stream()
+          .filter(e -> e.getName().endsWith(".class"))
+          .map(
+              e -> {
+                out.reset();
+                try (InputStream in = sample.getInputStream(e)) {
+                  int nRead;
+                  while ((nRead = in.read(buf, 0, buf.length)) != -1) {
+                    out.write(buf, 0, nRead);
+                  }
+                  return out.toByteArray();
+                } catch (IOException ignore) {
+                  return null;
+                }
+              })
+          .filter(Objects::nonNull)
+          .collect(Collectors.toList());
+    } catch (IOException ex) {
+      throw new UncheckedIOException(ex);
+    }
+  }
+}
