cfc95df0bfbd35f4ec4203a431a603cb1cfee8a3
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-08-07 10:43:25 +0100

    Introduce a compact membership filter for class-names

---
+++ class-match/src/main/java/datadog/instrument/classmatch/ClassNameFilter.java
@@ -0,0 +1,91 @@
+package datadog.instrument.classmatch;
+
+import java.util.Arrays;
+
+/**
+ * Compact filter that records class-name membership by their hash and short 'class-code'.
+ *
+ * <p>The 'class-code' includes the length of the package-prefix and simple-name, as well as the
+ * first and last characters of the simple-name. These elements coupled with the hash of the full
+ * class-name means the probability of a false-positive when testing membership is extremely low.
+ * (Testing the filter with class-names from 'GitHub Java Corpus' produced zero false-positives.)
+ * This means we can avoid storing full class-names, making the filter very compact.
+ */
+public final class ClassNameFilter {
+
+  private static final int MAX_CAPACITY = 1 << 20;
+  private static final int MIN_CAPACITY = 1 << 8;
+  private static final int MAX_HASH_ATTEMPTS = 10;
+
+  // fixed-size hashtable of encoded members, indexed by class-name
+  private final long[] members;
+  private final int slotMask;
+
+  public ClassNameFilter(int capacity) {
+    if (capacity < MIN_CAPACITY) {
+      capacity = MIN_CAPACITY;
+    } else if (capacity > MAX_CAPACITY) {
+      capacity = MAX_CAPACITY;
+    }
+    // choose enough slot bits to cover the given capacity
+    slotMask = -1 >>> Integer.numberOfLeadingZeros(capacity - 1);
+    members = new long[slotMask + 1];
+  }
+
+  /** Tests whether the given class-name is a member of the filter. */
+  public boolean contains(String className) {
+    int hash = className.hashCode();
+    for (int i = 1, h = hash; true; i++, h = rehash(h)) {
+      long codeHash = members[slotMask & h];
+      if (codeHash == 0) {
+        return false;
+      } else if ((int) codeHash == hash) {
+        // check hash first as it's cheap, then check class-code
+        return (int) (codeHash >>> 32) == classCode(className);
+      } else if (i == MAX_HASH_ATTEMPTS) {
+        return false;
+      }
+    }
+  }
+
+  /** Records the given class-name as a member of the filter. */
+  public void add(String className) {
+    int hash = className.hashCode();
+    // pack class-code and class-name hash into long (make hash easy to check)
+    long codeHash = (long) classCode(className) << 32 | 0xFFFFFFFFL & hash;
+    for (int i = 1, h = hash; true; i++, h = rehash(h)) {
+      int slot = slotMask & h;
+      if (members[slot] == 0) {
+        // use first slot that is empty
+        members[slot] = codeHash;
+        return;
+      } else if (i == MAX_HASH_ATTEMPTS) {
+        // go back and overwrite first slot
+        members[slotMask & hash] = codeHash;
+        return;
+      }
+    }
+  }
+
+  /** Removes all class-names from the filter. */
+  public void clear() {
+    Arrays.fill(members, 0);
+  }
+
+  /**
+   * Computes a 32-bit 'class-code' that includes the length of the package-prefix and simple-name,
+   * plus the first and last characters of the simple-name (each truncated to fit into 8-bits.)
+   */
+  private static int classCode(String className) {
+    int start = className.lastIndexOf('.') + 1;
+    int end = className.length() - 1;
+    int code = 0xFF & start;
+    code = (code << 8) | (0xFF & className.charAt(start));
+    code = (code << 8) | (0xFF & className.charAt(end));
+    return (code << 8) | (0xFF & (end - start));
+  }
+
+  private static int rehash(int oldHash) {
+    return Integer.reverseBytes(oldHash * 0x9e3775cd) * 0x9e3775cd;
+  }
+}
