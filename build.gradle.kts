77f0389b105b1f6816b0b7d790142a5afd2a212c
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-08-19 23:52:51 +0100

    Aggregate subproject content into build/libs/dd-instrument-java-<version>.jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,25 @@
 plugins {
+  java
+
   id("pl.allegro.tech.build.axion-release")
 }
 
 version = scmVersion.version
+
+repositories {
+  mavenCentral()
+}
+
+// collect all subproject output into a single jar
+val embed by configurations.creating {
+  isTransitive = false
+}
+dependencies {
+  embed(project(":utils"))
+  embed(project(":class-inject"))
+  embed(project(":class-match"))
+}
+tasks.jar {
+  dependsOn(embed)
+  from(embed.map { zipTree(it) })
+}
