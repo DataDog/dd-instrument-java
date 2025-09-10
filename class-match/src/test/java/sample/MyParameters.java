2eb3142b9d117b5865ddba8f90eeccd0600206cd
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-09-10 16:56:10 +0100

    Test method parameter parsing

---
+++ class-match/src/test/java/sample/MyParameters.java
@@ -0,0 +1,31 @@
+package sample;
+
+import static java.util.Collections.emptyMap;
+
+import java.util.Map;
+
+public class MyParameters {
+  @SuppressWarnings("unchecked")
+  public Map<String, String>[] method(
+      boolean a1,
+      byte a2,
+      char a3,
+      double a4,
+      float a5,
+      int a6,
+      long a7,
+      short a8,
+      String a9,
+      boolean[] a10,
+      byte[][] a11,
+      char[][][] a12,
+      double[][][][] a13,
+      float[][][][][] a14,
+      int[][][][][][] a15,
+      long[][][][][][][] a16,
+      short[][][][][][][][] a17,
+      Map<Integer, Float> a18,
+      String[] a19) {
+    return new Map[] {emptyMap()};
+  }
+}
