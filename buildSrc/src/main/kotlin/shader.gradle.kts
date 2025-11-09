c2fecd8c3836a552bc102463bc4272bd5c6d729a
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-11-09 01:24:18 +0000

    Upgrade ASM class-files to target Java 8 (with stack-maps for faster loading)

--- buildSrc/src/main/kotlin/shader.gradle.kts
+++ buildSrc/src/main/kotlin/shader.gradle.kts
@@ -1,4 +1,5 @@
 import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.commons.ClassRemapper
@@ -8,6 +9,14 @@ import java.io.Reader
 import java.io.StringReader
 import java.nio.charset.StandardCharsets
 
+class ToJava8(cv: ClassVisitor) : ClassVisitor(Opcodes.ASM9, cv) {
+  override fun visit(
+    version: Int, access: Int, name: String?, signature: String?, superName: String?, interfaces: Array<out String?>?
+  ) {
+    super.visit(version.coerceAtLeast(52), access, name, signature, superName, interfaces)
+  }
+}
+
 class Shade(reader: Reader) : FilterReader(shade(reader)) {
   companion object {
     fun shade(reader: Reader): Reader {
@@ -15,6 +24,7 @@ class Shade(reader: Reader) : FilterReader(shade(reader)) {
         override fun map(internalName: String?): String? {
           return internalName?.replace("org/objectweb/", "datadog/instrument/")
         }
+
         override fun mapValue(value: Any?): Any? {
           if (value is String) {
             return value.replace("org/objectweb/", "datadog/instrument/")
@@ -25,7 +35,7 @@ class Shade(reader: Reader) : FilterReader(shade(reader)) {
       }
       val cr = ClassReader(reader.readText().toByteArray(StandardCharsets.ISO_8859_1))
       val cw = ClassWriter(ClassWriter.COMPUTE_FRAMES)
-      cr.accept(ClassRemapper(cw, remapper), 0)
+      cr.accept(ClassRemapper(ToJava8(cw), remapper), 0)
       return StringReader(String(cw.toByteArray(), StandardCharsets.ISO_8859_1))
     }
   }
